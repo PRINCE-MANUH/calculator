d9c2b28cf8fc31e344f1edac56d7e2bec06b9763
PRINCE-MANUH <108537367+PRINCE-MANUH@users.noreply.github.com>
2022-07-07 09:24:43 -0700

    Add files via upload

---
+++ calculator/cal.js
@@ -0,0 +1,21 @@
+let result=document.getElementById("inputext");
+
+let calculate=(number)=>{
+    result.value+=number
+}
+let Result=()=>{
+    try{
+        result.value=eval(result.value)
+    }
+    catch(err){
+        alert(" Please enter valid input");
+    }
+}
+
+function clr(){
+    result.value=" ";
+}
+
+function del(){
+    result.value=result.value.slice(0,-1);
+    }
